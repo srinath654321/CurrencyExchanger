51b4f4933111eec1da51720a64f65de7c9bbc8d3
srikavuri <srinath.kavuri@gmail.com>
2020-10-21 11:04:27 -0400

    new changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CurrencyConverter
+# CurrencyExchanger
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.8.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/CurrencyConverter'),
+      dir: require('path').join(__dirname, './coverage/CurrencyExchanger'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
